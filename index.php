c79566e4a4ebbb0459c6b66c0d11966e11e0b502
barcellos88 <67702961+barcellos88@users.noreply.github.com>
2021-03-29 12:57:16 -0300

    adicionado validação de campos e exibição de mensagem de retorno

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,6 +16,23 @@
 <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
 <form action="script.php" method="post">
+    <?php
+                          //isset=verificação se variavel existe e se valor !=null
+                          //"?" no meio equivale ao "if" e os ":" equivale ao "else"
+        $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : ''; 
+        if(!empty($mensagemDeErro))
+        {
+            echo $mensagemDeErro;
+        }
+
+        $mensagemDeSucesso = isset($_SESSION['mensagem-de-sucesso']) ? $_SESSION['mensagem-de-sucesso'] : ''; 
+        if(!empty($mensagemDeSucesso))
+        {
+            echo $mensagemDeSucesso;
+        }
+    ?>
+    
+
     <p>Seu Nome: <input type="text" name="nome" /></p>
     <p>Sua Idade: <input type="text" name="idade" /></p>
     <p><input type="submit" value="Enviar dados do competidor"></p>
--- script.php
+++ script.php
@@ -4,66 +4,106 @@ em qual categoria o nadador erá competir, por exemplo, 6 a 12 anos categoria in
 de 13 a 18 categoria adolescente, acima de 18 categoria adulto, o sistema deverá retornar 
 a categoria para cada nadador que for cadastrado. -->
 
-<!--2º parte Crie um Formulário para que o usuário possa através de uma página web preencher o nome 
+<!-- 2º parte Crie um Formulário para que o usuário possa através de uma página web preencher o nome 
 e a idade dos competidores. Esses dados deverão ser utilizados para exibir em qual categoria 
 o competidor se encaixa.-->
 <?php
 
+session_start();
+
 $categorias   = [];
-$categorias[] = 'Infantil';
-$categorias[] = 'Adolescente';
-$categorias[] = 'Adulto'; 
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto'; 
 
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+//inicio das verificações
 if(empty($nome))
 {
+    /*
+        //1º forma de verificação de erros
     echo 'O nome não pode ser vazio';
     return;
+    */
+    $_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio';
+    header( 'location: index.php');//faz com q depois do erro, volte para o index e nao fique preso no script.php
+    return;
+    
 }
-
-if(strlen($nome) < 3)
+else if(strlen($nome) < 3)//nao deixa ser digitado menos doque 3 caracteres
 {
-    echo 'O nome deve conter mais de 3 caracteres';
+    /*echo 'O nome deve conter mais de 3 caracteres';
+    return;*/
+    $_SESSION['mensagem-de-erro'] = 'O nome não pode conter menos de 3 caracteres';
+    header( 'location: index.php');
     return;
 }
-
-if(strlen($nome) > 40)
+else if(strlen($nome) > 40)//limita a quantidade de caracteres digitados
 {
-    echo 'O nome é muito extenso';
+    /*echo 'O nome é muito extenso';
+    return;*/
+    $_SESSION['mensagem-de-erro'] = 'O nome é muito extenso';
+    header( 'location: index.php');
     return;
 }
-
-if(!is_numeric($idade))
+else if(!is_numeric($idade))//verifica se foi digitado numeros ao invés de letras
 {
-    echo "Informe um número para idade";
+    /*echo "Informe um número para idade";
+    return;*/
+    $_SESSION['mensagem-de-erro'] = 'Informe um número para idade';
+    header( 'location: index.php');
     return;
 }
+//termino das verificações
+
 
 if ($idade>=6 && $idade<=12)
 {
-    for ($i=0; $i<=count($categorias); $i++)
+    for($i=0; $i<=count($categorias); $i++)
     {
-        if($categorias[$i] == "Infantil")
-            echo "O nadador ".$nome." Compete na categoria ".$categorias[$i];
+        if($categorias[$i] == 'infantil')
+        {
+            /*  //1º forma de apresentar a mensagem
+                //com "" nao precisa de ponto para separar variaveis
+            //echo "O nadador $nome Compete na categoria $categorias[$i]";
+            */
+            $_SESSION['mensagem-de-sucesso'] = "O nadador $nome Compete na categoria $categorias[$i]";
+            header("location: index.php");
+            return;
+        }
     }
 }
-elseif($idade>=13 && $idade<=18)
+else if($idade>=13 && $idade<=18)
 {
-    for ($i=0; $i<=count($categorias); $i++)
+    for($i=0; $i<=count($categorias); $i++)
     {
-        if($categorias[$i] == 'Adolescente')
-            echo "O nadador ".$nome." Compete na categoria ".$categorias[$i];
+        if($categorias[$i] == 'adolescente')
+        {
+            /*
+            //com '' precisa usar ponto
+            echo 'O nadador '.$nome.' Compete na categoria '.$categorias[$i];
+            */
+            $_SESSION['mensagem-de-sucesso'] = 'O nadador '.$nome.' Compete na categoria '.$categorias[$i];
+            header("location: index.php");
+            return;
+        
+        }
     }
 }
 else
 {
-    for ($i=0; $i<=count($categorias); $i++)
+    for($i=0; $i<=count($categorias); $i++)
     {
-        if($categorias[$i] == 'Adulto')
-            echo "O nadador ".$nome." Compete na categoria ".$categorias[$i];
+        if($categorias[$i] == 'adulto')
+        {
+            //echo "O nadador ".$nome." Compete na categoria ".$categorias[$i];
+            $_SESSION['mensagem-de-sucesso'] = "O nadador $nome Compete na categoria $categorias[$i]";
+            header("location: index.php");
+            return;
+        }
     }
 }
 
